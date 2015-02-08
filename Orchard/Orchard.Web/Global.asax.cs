00f0f3736285d5dde4dc040ec47f3cde8d05f8a5
ahmedhelmy204 <ahmedhelmy204@hotmail.com>
2015-02-08 13:41:51 +0200

    Add Projects (Orchard.Framework, Orchard.WarmupStarter) . Add Interface IOrchardHost. Add Class Starter.

---
+++ Orchard/Orchard.Web/Global.asax.cs
@@ -0,0 +1,60 @@
+﻿using Orchard.Environment;
+using Orchard.WarmupStarter;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Security;
+using System.Web.SessionState;
+
+namespace Orchard.Web
+{
+    // Note: For instructions on enabling IIS6 or IIS7 classic mode, 
+    // visit http://go.microsoft.com/?LinkId=9394801
+
+    public class MvcApplication : System.Web.HttpApplication
+    {
+        private static Starter<IOrchardHost> _starter;
+
+        // class constructor
+        public MvcApplication()
+        {
+
+        }
+
+        protected void Application_Start(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Session_Start(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Application_BeginRequest(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Application_AuthenticateRequest(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Application_Error(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Session_End(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Application_End(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
