b6066c49bb8a915089dd8a4280edf8a547232af9
ahmedhelmy204 <ahmedhelmy204@hotmail.com>
2015-02-09 10:38:54 +0200

    Add Autofac.dll

--- Orchard/Orchard.Framework/Environment/IOrchardHost.cs
+++ Orchard/Orchard.Framework/Environment/IOrchardHost.cs
@@ -17,17 +17,17 @@ namespace Orchard.Environment
         /// Called externally when there is explicit knowledge that the list of installed
         /// modules/extensions has changed, and they need to be reloaded.
         /// </summary>
-        void ReloadExtensions();
+        //void ReloadExtensions();
 
         /// <summary>
         /// Called each time a request begins to offer a just-in-time reinitialization point
         /// </summary>
-        void BeginRequest();
+        //void BeginRequest();
 
         /// <summary>
         /// Called each time a request ends to deterministically commit and dispose outstanding activity
         /// </summary>
-        void EndRequest();
+        //void EndRequest();
 
         //ShellContext GetShellContext(ShellSettings shellSettings);
 
--- Orchard/Orchard.WarmupStarter/Starter.cs
+++ Orchard/Orchard.WarmupStarter/Starter.cs
@@ -3,12 +3,18 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Web;
 
 namespace Orchard.WarmupStarter
 {
     public class Starter<T> where T : class
     {
+        private readonly Func<HttpApplication, T> _initialization;
 
+        public Starter(Func<HttpApplication,T> initialization)
+        {
+            _initialization = initialization;
+        }
 
     }
 }
